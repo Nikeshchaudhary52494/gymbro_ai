02b60a1bf74d1811a1b0dee2298ddb16378c5574
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2026-03-20 22:20:32 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-bold text-5xl flex items-center justify-center h-screen text-white bg-black">
+      <h1>FITBOTAI</h1>
+    </div>
+  );
+}
